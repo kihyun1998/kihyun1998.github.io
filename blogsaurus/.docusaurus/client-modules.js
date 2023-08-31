973720e87c0c856368c79145f4da6b5448b42eee
wantSome <kihyun1998@naver.com>
2023-08-31 20:37:54 +0900

    gogo

---
+++ blogsaurus/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\kihyun1998.github.io\\blogsaurus\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\kihyun1998.github.io\\blogsaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\kihyun1998.github.io\\blogsaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\kihyun1998.github.io\\blogsaurus\\src\\css\\custom.css'),
+];
