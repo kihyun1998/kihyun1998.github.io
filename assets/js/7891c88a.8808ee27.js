1c821e936e11168ba00288916f195ec69c1f366a
kihyun1998 <kihyun1998@users.noreply.github.com>
2023-12-28 06:25:18 +0000

    deploy: d3348d141b77e7c817671f9f631c4943adc54bae

---
+++ assets/js/7891c88a.8808ee27.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblogsaurus=self.webpackChunkblogsaurus||[]).push([[7622],{65165:s=>{s.exports=JSON.parse('{"permalink":"/blog/tags/future-wait","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
