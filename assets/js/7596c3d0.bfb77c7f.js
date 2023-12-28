3d06a745990d185cd97a2408b72e2b13c18ec644
kihyun1998 <kihyun1998@users.noreply.github.com>
2023-12-28 01:14:47 +0000

    deploy: 5ff4357f5ca77b95f4ad1771456e56a683e433cd

---
+++ assets/js/7596c3d0.bfb77c7f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblogsaurus=self.webpackChunkblogsaurus||[]).push([[7775],{78754:a=>{a.exports=JSON.parse('{"label":"future_wait","permalink":"/blog/tags/future-wait","allTagsPath":"/blog/tags","count":1}')}}]);
